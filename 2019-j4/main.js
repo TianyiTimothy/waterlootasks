5bfdc40b0d9f3bfd5ea9d0a748adc9c52f0c09d1
TimothyZhang <TimothyZhang@foxmail.com>
2020-05-22 23:53:51 +0800

    update J42019 solution with a little usability

--- 2019-j4/main.js
+++ 2019-j4/main.js
@@ -1,30 +1,45 @@
-window.onload=()=>{
+window.onload = () => {
 
-  let originArr = [[1,2],[3,4]];
-  let currArr = originArr;
+  // vars
+  let originArr = [[1, 2], [3, 4]];
+  let userInputTextDOM = document.getElementById('userInputText');
+  let currArr;
+  let userInputText;
 
-  let funcH = (arr)=>{
+  let init = ()=>{
+    currArr = originArr;
+    userInputText = '';
+  }
+
+
+  let funcH = (arr) => {
     // arr[0][0] = [arr[1][0],arr[1][0]=arr[0][0]][0];
-    arr = [arr[1],arr[0]];
+    arr = [arr[1], arr[0]];
     return arr;
   };
   // console.log(funcH(originArr));
-  let funcV = (arr)=>{
-    arr = [[arr[0][1],arr[0][0]],[arr[1][1],arr[1][0]]];
-    return arr;};
+  let funcV = (arr) => {
+    arr = [[arr[0][1], arr[0][0]], [arr[1][1], arr[1][0]]];
+    return arr;
+  };
 
-  let getOutput = ()=>{
+  let getOutput = () => {
     let inputText = document.getElementById('text').value;
-    for (c of inputText){
-      if(c==="H"){
+    if(inputText===''){
+      inputText = userInputText===''?'HV':userInputText;
+    }
+    init();
+    showUserInputText(inputText);
+    for (c of inputText) {
+      if (c === "H") {
         currArr = funcH(currArr);
-      }else if(c==="V"){
+      } else if (c === "V") {
         currArr = funcV(currArr);
       }
     }
   }
 
-  let displayOutput = (arr)=>{
+  let displayOutput = (arr) => {
     const spotADOM = document.getElementById('spotA');
     const spotBDOM = document.getElementById('spotB');
     const spotCDOM = document.getElementById('spotC');
@@ -35,13 +50,34 @@ window.onload=()=>{
     spotDDOM.innerText = arr[1][1];
   };
 
-  let setOutput = ()=>{
+  let goClick = () => {
     getOutput();
     displayOutput(currArr);
   };
 
+  init();
   displayOutput(originArr);
 
+  // additional below
+  let hClick = () => {
+    currArr = funcH(currArr);
+    displayOutput(currArr);
+    showUserInputText('H');
+  };
+  let vClick = () => {
+    currArr = funcV(currArr);
+    displayOutput(currArr);
+    showUserInputText('V');
+  };
+  let showUserInputText = (text)=>{
+    userInputText += text;
+    // show it in html.. really miss data binding stuff
+    userInputTextDOM.innerHTML = userInputText;
+  }
+
+
   // add event listener on validation button
-  document.getElementById('valBtn').addEventListener('click', setOutput);
+  document.getElementById('goBtn').addEventListener('click', goClick);
+  document.getElementById('hBtn').addEventListener('click', hClick);
+  document.getElementById('vBtn').addEventListener('click', vClick);
 }
